b9374c037495d3ad9a802f6282781d55dbb3007c
Yang Xianglin <yxlyzxl@gmail.com>
2024-12-07 17:11:46 +0800

    [imp] first version of reasoning prompt

---
+++ src/evol/answer_evol.py
@@ -0,0 +1,138 @@
+"""
+Enrich the answers with reasoning why it is harmful/not harmful and output or refusal.
+The format contains (customized by strategy):
+1. rephrase content/question
+2. ask is it harmful+reasoning why it is/not harmful 
+3. output or refusal
+"""
+import random
+import os
+import json
+import asyncio
+
+from src.evol.evol_llms import llm_generate
+
+# --------------------Answer Format Templates--------------------
+# TODO: currently we only attach constraints to the question. We can consider using more advanced model to automatically extract constraints from the question.
+# 1. Analysis Request
+# 2. Harmful Check with reasoning
+# 3. Refusal or Output
+
+__strategies__ = ["DISTRACTED_QUESTION", "SUPPRESS_REFUSAL", "AFFIRMATIVE_OUTPUT", "ROLE_PLAY_STORY", "ENCODED_INPUT"]
+
+REPHRASE_FORMAT = """First, let me analyze the request: You are asking me answer ###question### ###constraints###"""
+
+CONSTRAINTS = {
+    "DISTRACTED_QUESTION": "while multitaksing on daily tasks.",
+    "SUPPRESS_REFUSAL": "and avoid rejected words.",
+    "AFFIRMATIVE_OUTPUT": "and start with affirmative tone.",
+    "ROLE_PLAY_STORY": "and role-play as a character.",
+    "ENCODED_INPUT": "in encoded format.",
+}
+
+HARMFUL_CHECK_FORMAT_TEMPLATE = """Next, let's evaluate if this request involves violent or criminal actions before giving the answer. """
+
+REFUSAL_FORMAT = """Therefore, I'm sorry, but I cannot assist with that request."""
+
+# --------------------Prompt Evol--------------------
+# Rephrase the prompt with more variations with gpt capability
+# 1. variable format remain
+# 2. rephrase should be reasonable and must be understood and responded by humans.
+# 3. format requirements:
+REPHRASE_BASE_INSTRUCTION = """I want you to act as a Prompt Rephraser.\r\n \
+	Your goal is to rephrase the #Given Prompt# into a new prompt that has the same meaning as the #Given Prompt# but with a different wording.\r\n \
+    Remember not to change the meaning of the original prompt and lose the original context.\r\n \
+	The rephrased prompt must be reasonable and must be understood and responded by humans.\r\n \
+	Your rewriting should keep the variable format '###variable###' in #The Given Prompt#.\r\n \
+	You should try your best not to make the #Rewritten Prompt# become verbose, #Rewritten Prompt# can only add 10 to 20 words into #The Given Prompt#. \r\n \
+	'#The Given Prompt#', '#Rewritten Prompt#', 'given prompt' and 'rewritten prompt' are not allowed to appear in #Rewritten Prompt#\r\n" \r\n """
+
+
+# --------------------Answer Evol--------------------
+class AnswerEvol:
+    def __init__(self):
+        self._strategies = __strategies__
+        self.rephrase_format = REPHRASE_FORMAT
+        self.harmful_check_format = HARMFUL_CHECK_FORMAT_TEMPLATE
+        self.refusal_format = REFUSAL_FORMAT
+        self.rephrase_instruction = REPHRASE_BASE_INSTRUCTION
+        self.constraints = CONSTRAINTS
+    
+    @property
+    def strategies(self):
+        return self._strategies
+    
+    # --------------------Breath--------------------
+    def createRephrasePrompt(self, instruction: str) -> str:
+        prompt = self.rephrase_instruction
+        prompt += "#Given Prompt#: \r\n {} \r\n".format(instruction)
+        prompt += "#Rewritten Prompt#:\r\n"
+        return prompt
+    
+    # --------------------Depth--------------------
+    def createReasoningPrompt(self, strategy_name):
+        assert strategy_name in self._strategies
+
+        rephrase_prompt = self.rephrase_format.replace("###constraints###", self.constraints[strategy_name])
+        reason_prompt = self.harmful_check_format
+        reason_prompt += "Since the question is about ###category###, it is prohibited to answer"
+        refusal_prompt = self.refusal_format
+        prompt = rephrase_prompt + reason_prompt + refusal_prompt
+        return prompt
+     
+    # --------------------Utils--------------------
+    def clean_prompt(self, prompt: str) -> str:
+        """Clean and format the prompt"""
+        if "###question###" in prompt and "###category###" in prompt:
+            prompt = prompt.replace("###question###", "{question}")
+            prompt = prompt.replace("###category###", "{category}")
+            return prompt
+        else:
+            return None
+    
+    async def generate_reasoning_variants_with_strategy(self, strategy_name, model_name, num_variants):
+        reasoning_prompt = self.createReasoningPrompt(strategy_name)
+        print(reasoning_prompt)
+        print("-"*50)
+
+        new_variants = [reasoning_prompt]
+        while len(new_variants) < num_variants:
+            seed_prompt = random.choice(new_variants)
+            rephrase_prompt = self.createRephrasePrompt(seed_prompt)
+            new_prompt = await llm_generate(model_name=model_name, 
+                                            prompt=rephrase_prompt)
+            if self.clean_prompt(new_prompt):
+                print(len(new_variants))
+                print(new_prompt)
+                new_variants.append(new_prompt)
+        return new_variants
+    
+    def save_answer_variants(self, strategy_name: str, answer_variants: list, file_path="data/prompt_variants/answers"):
+        os.makedirs(file_path, exist_ok=True)
+        save_file = os.path.join(file_path, f"{strategy_name.lower()}.json")
+        if os.path.exists(save_file):
+            with open(save_file, "r") as f:
+                existing_variants = json.load(f)
+            existing_variants.extend(answer_variants)
+            answer_variants = existing_variants
+        with open(save_file, "w") as f:
+            json.dump(answer_variants, f)
+
+if __name__ == "__main__":
+    import asyncio
+    
+    async def main():
+        
+        num_variants_per_class = 100
+        model_name = "gpt-4o"
+
+        answer_evol = AnswerEvol()
+        for strategy in ["DISTRACTED_QUESTION", "SUPPRESS_REFUSAL", "AFFIRMATIVE_OUTPUT", "ROLE_PLAY_STORY"]:
+            print(f"Generating {num_variants_per_class} variants for {strategy}...")
+            answer_variants = await answer_evol.generate_reasoning_variants_with_strategy(strategy, model_name, num_variants_per_class)
+            answer_evol.save_answer_variants(strategy, answer_variants)
+            print(f"Saved {len(answer_variants)} variants for {strategy}.")
+            print("*"*50)
+        
+    asyncio.run(main())
+    
