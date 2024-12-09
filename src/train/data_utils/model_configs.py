3c5e9fb8355c832ec5511a49510b7756d3ba0eff
Yang Xianglin <yxlyzxl@gmail.com>
2024-12-09 13:41:52 +0800

    [fix] minor

--- src/train/data_utils/model_configs.py
+++ src/train/data_utils/model_configs.py
@@ -5,20 +5,20 @@
 LLAMA2_DEFAULT_SYSTEM_PROMPT = """You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
 If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
 LLAMA2_CHAT_TEMPLATE = "[INST] {instruction} [/INST] "
-LLAMA2_CHAT_TEMPLATE_WITH_SYSTEM = "[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n{instruction} [/INST] "
+LLAMA2_CHAT_TEMPLATE_WITH_SYSTEM = "[INST] <<SYS>>\n{}\n<</SYS>>\n\n{instruction} [/INST] "
 LLAMA2_USER_TAG = "[INST] "
 LLAMA2_ASSISTANT_TAG = " [/INST]"
 LLAMA2_SEP_TOKEN = " "
-LLAMA2_SYSTEM = "<<SYS>>\n{system_prompt}\n<</SYS>>\n\n".format(LLAMA2_DEFAULT_SYSTEM_PROMPT)
+LLAMA2_SYSTEM = "<<SYS>>\n{}\n<</SYS>>\n\n".format(LLAMA2_DEFAULT_SYSTEM_PROMPT)
 
 # Llama 3 chat templates are based on
 # - https://llama.meta.com/docs/model-cards-and-prompt-formats/meta-llama-3/
 # <|begin_of_text|> is automatically added by the tokenizer
 LLAMA3_CHAT_TEMPLATE = """<|start_header_id|>user<|end_header_id|>{instruction}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
-LLAMA3_CHAT_TEMPLATE_WITH_SYSTEM = """<|start_header_id|>system<|end_header_id|>{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>{instruction}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
+LLAMA3_CHAT_TEMPLATE_WITH_SYSTEM = """<|start_header_id|>system<|end_header_id|>{}<|eot_id|><|start_header_id|>user<|end_header_id|>{instruction}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
 LLAMA3_USER_TAG="<|start_header_id|>user<|end_header_id|>\n\n"
 LLAMA3_ASSISTANT_TAG="<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
-LLAMA3_SYSTEM = "<|start_header_id|>system<|end_header_id|>{system_prompt}<|eot_id|>".format(LLAMA2_DEFAULT_SYSTEM_PROMPT)
+LLAMA3_SYSTEM = "<|start_header_id|>system<|end_header_id|>{}<|eot_id|>".format(LLAMA2_DEFAULT_SYSTEM_PROMPT)
 LLAMA3_SEP_TOKEN = ""
 
 # TODO Fix Mistral
