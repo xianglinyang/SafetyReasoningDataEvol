49aa7db1d66fb3634fc553517615e0e8836c91e2
Yang Xianglin <yxlyzxl@gmail.com>
2024-12-11 20:42:42 +0800

    [imp] todo

---
+++ src/evaluate/adv_attack.py
@@ -0,0 +1,27 @@
+'''
+This file is used to evaluate the adversarial attack on the reasoning dataset.
+1. None
+2. Manual
+3. AutoDAN
+4. TAP-T
+5. PAIR
+6. GCG
+7. Multilingual
+8. Prefilling
+9. Input Embed
+10. RepE Attack
+'''
+__adv_attacks__ = [
+    "None",
+    "Manual",
+    "AutoDAN",
+    "TAP-T",
+    "PAIR",
+    "GCG",
+    "Multilingual",
+    "Prefilling",
+    "Input Embed",
+    "RepE Attack"
+]
+
+# TODO: add the attack methods
