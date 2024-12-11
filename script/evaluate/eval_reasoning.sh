3b25b3166eecf04d06d69f3cbe2506e0ce0ab9b0
Yang Xianglin <yxlyzxl@gmail.com>
2024-12-11 20:34:08 +0800

    [imp] script to eval common loss

---
+++ script/evaluate/eval_reasoning.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=0
+
+model_name_or_path_list=(
+    "meta-llama/Llama-2-7b-chat-hf"
+    "meta-llama/Llama-3.1-8B-Instruct"
+    "mistralai/Mistral-7B-Instruct-v0.1"
+    "outputs/circuitbreaker/llama2-7b"
+    "outputs/circuitbreaker/llama3-8b"
+    "outputs/circuitbreaker/mistral-7b"
+)
+dataset_name_list=(
+    "mmlu"
+    "gsm8k"
+)
+
+header="torchrun --nproc_per_node=1 -m src.evaluate.evaluate_common_reasoning"
+base_arguments="\
+--torch_type bf16 \
+--split test \
+--eval_num 500 \
+--device cuda"
+
+for model_name_or_path in ${model_name_or_path_list[@]}; do
+    for dataset_name in ${dataset_name_list[@]}; do
+        full_command="$header $base_arguments \
+        --model_name_or_path $model_name_or_path \
+        --dataset_name $dataset_name"
+        echo $full_command
+        eval $full_command
+    done
+done
+
