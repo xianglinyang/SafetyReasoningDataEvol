e77c95ff084ccec9c76abfc8cda78d174431ba6f
Yang Xianglin <yxlyzxl@gmail.com>
2024-12-12 10:58:20 +0800

    [update] script to run evaluate in parallel

--- script/evaluate/eval_reasoning.sh
+++ script/evaluate/eval_reasoning.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
+
+# export CUDA_VISIBLE_DEVICES=0
+jobs_num=2  # number of jobs to run in parallel, jobs_num = gpu_num*per_gpu_jobs_num
+gpu_num=2  # number of GPUs
 
 model_name_or_path_list=(
     "meta-llama/Llama-2-7b-chat-hf"
@@ -21,13 +24,24 @@ base_arguments="\
 --eval_num 500 \
 --device cuda"
 
+# Counter to distribute commands across GPUs
+counter=0
+
 for model_name_or_path in ${model_name_or_path_list[@]}; do
     for dataset_name in ${dataset_name_list[@]}; do
-        full_command="$header $base_arguments \
-        --model_name_or_path $model_name_or_path \
-        --dataset_name $dataset_name"
-        echo $full_command
-        eval $full_command
+        gpu_id=$((counter % gpu_num))
+        run_id=$RANDOM
+
+        CUDA_VISIBLE_DEVICES=$gpu_id $header $base_arguments \
+            --model_name_or_path $model_name_or_path \
+            --dataset_name $dataset_name \
+            --run_id $run_id &
+
+        counter=$((counter + 1))
+
+        if [ $((counter % jobs_num)) -eq 0 ]; then
+            wait
+        fi
     done
 done
 
--- script/evaluate/prompt_attack.sh
+++ script/evaluate/prompt_attack.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
+# export CUDA_VISIBLE_DEVICES
+jobs_num=2
+gpu_num=2
 
 model_name_or_path_list=(
-    "meta-llama/Llama-2-7b-chat-hf"
+    # "meta-llama/Llama-2-7b-chat-hf"
     "meta-llama/Llama-3.1-8B-Instruct"
     "mistralai/Mistral-7B-Instruct-v0.1"
     "outputs/circuitbreaker/llama2-7b"
@@ -10,14 +12,14 @@ model_name_or_path_list=(
     "outputs/circuitbreaker/mistral-7b"
 )
 dataset_name_list=(
-    "sorrybench"
+    # "sorrybench"
     "jailbreakbench"
-    "advbench"
+    # "advbench"
 )
 attack_name_list=(
     "none"
     "refusal_suppression"
-    "prefix_injection"
+    # "prefix_injection"
 )
 
 
@@ -26,18 +28,37 @@ base_arguments="\
 --split train \
 --device cuda \
 --torch_type bf16 \
---eval_num 500"
+--eval_num 200"
+
+# Counter to distribute commands across GPUs
+counter=0
 
 for model_name_or_path in ${model_name_or_path_list[@]}; do
     for dataset_name in ${dataset_name_list[@]}; do
         for attack_name in ${attack_name_list[@]}; do
-            # Run the Python script with arguments
-            full_command="$header $base_arguments \
+            # Calculate GPU ID (0 or 1) and slot (0 or 1) for each command
+            gpu_id=$((counter % gpu_num))
+            
+            # generate a random job id for each command
+            run_id=$RANDOM
+            
+            # Construct and run the command in background
+            CUDA_VISIBLE_DEVICES=$gpu_id $header $base_arguments \
                 --model_name_or_path ${model_name_or_path} \
                 --dataset_name ${dataset_name} \
-                --attack_name ${attack_name}"
-            echo $full_command
-            eval $full_command
+                --attack_name ${attack_name} \
+                --run_id "$run_id" &
+            
+            # Increment counter
+            counter=$((counter + 1))
+            
+            # If we've launched jobs_num jobs, wait for them to complete
+            if [ $((counter % jobs_num)) -eq 0 ]; then
+                wait
+            fi
         done
     done
 done
+
+# Wait for any remaining background jobs
+wait
