051c7a3d8471bc53565fdf076279f36b5bfedb3e
Yang Xianglin <yxlyzxl@gmail.com>
2024-12-21 18:25:47 +0800

    [imp] more enhanced safe cot

--- src/evol/answers.py
+++ src/evol/answers.py
@@ -1,117 +1,168 @@
 '''
-Enrich the answers with reasoning why it is harmful/not harmful and output or refusal.
-The format contains (customized by strategy):
-1. rephrase content/question
-2. ask is it harmful+reasoning why it is/not harmful 
-3. output or refusal
+Enrich the answers with reasoning why it is harmful/not harmful and output or refusal with 4o-mini.
 '''
+import logging
+from tqdm import tqdm
+from src.llm_zoo.api_base_models import OpenAILLM
+logger = logging.getLogger(__name__)
+# ----------Rules----------
+Rules = [
+    "illicit/violent",
+    "sexual/minors",
+    "harassment/threatening",
+    "hate/threatening",
+    "self-harm/intent/instructions",
+    "violence/graphic",
+    "privacy/sensitive",
+    "misinformation",
+]
+# ----------Prompt----------
 
-import random
-import os
-import json
-from src.evol.questions import __strategies__
+safe_cot_format = """#### Verify Request
+I need to verify the dangerousness of the user's request to [summary] with following categories before generating the response.
 
-# ----------Format----------
-refusal_format = """\
-#### Verify Request
-This is a request asking to output involving {category}, which would lead to unsafe content and is prohibited.
+* harassment/threatening:[reason]
+* hate/threatening:[reason]
+* illicit/violent:[reason]
+* self-harm/intent/instructions:[reason]
+* sexual/minors:[reason]
+* violence/graphic:[reason]
+* misinformation:[reason]
+* privacy/sensitive:[reason]
 
-#### Respond
+Therefore, the user's request likely involves a clear reference to {categories}, which could lead to unsafe behavior. I need to reject the request.
+
+#### Response
 {output}
 """
 
-normal_format = """\
+safe_cot_normal_format = """\
 #### Verify Request
-This is a request asking to output involving {category}.
+I need to verify the dangerousness of the user's request to {summary} with following categories before generating the response.
 
-#### Respond
-{output}
+* harassment/threatening: Mismatch.
+* hate/threatening: Mismatch.
+* illicit/violent: Mismatch.
+* self-harm/intent/instructions: Mismatch.
+* sexual/minors: Mismatch.
+* violence/graphic: Mismatch.
+* misinformation: Mismatch.
+* privacy/sensitive: Mismatch.
+
+Therefore, the user's request is safe and I can generate the response.
 """
+
+safe_cot_prompt = """#### Instruction \
+You are a helpful assistant. \
+You are helping to analyze a harmful question following the following steps:\n \
+1) summarize the question with one phrase less than 10 words. \
+2) classify the question into categories and give explanation. \
+3) verify the dangerousness of the question. \
+
+You SHOULD strictly output with the follow the following format: \
+'#### Verify Request
+I need to verify the dangerousness of the user's request to [summary] with following categories before generating the response.
+
+* harassment/threatening:[reason]
+* hate/threatening:[reason]
+* illicit/violent:[reason]
+* self-harm/intent/instructions:[reason]
+* sexual/minors:[reason]
+* violence/graphic:[reason]
+* misinformation:[reason]
+* privacy/sensitive:[reason]
+
+Therefore, the user's request likely involves a clear reference to [categories, which could lead to unsafe behavior. I need to reject the request.'
+For the reason, if mismatch, output 'Mismatch.'. If match, output the reason with one sentence.
+#### Question 
+{question} 
+#### Answer\n"""
+
+summary_prompt = """#### Instruction \
+You are a helpful assistant. \
+You are helping to summarize a given question with one phrase less than 10 words.
+
+#### Question
+{question}
+
+#### Summary
+"""
+
+# ----------Enrich Function----------
+def batch_get_summary(questions: list[str], model_name: str = "gpt-4o-mini", max_tokens: int = 2048, temperature: float = 0.7) -> str:
+	summary_prompts = [summary_prompt.format(question=question) for question in questions]
+	llm = OpenAILLM(model_name=model_name, 
+				 	temperature=temperature, 
+				 	max_tokens=max_tokens)
+	summary_responses = list()
+	for i in tqdm(range(len(summary_prompts))):
+		summary_responses.append(llm.invoke(summary_prompts[i]))
+	return summary_responses
+
+
+def batch_get_safe_cot(questions: list[str], model_name: str = "gpt-4o-mini", max_tokens: int = 2048, temperature: float = 0.7) -> str:
+	safe_cot_prompts = [safe_cot_prompt.format(question=question) for question in questions]
+	llm = OpenAILLM(model_name=model_name, 
+				 	temperature=temperature, 
+				 	max_tokens=max_tokens)
+	safe_cot_responses = list()
+	for i in tqdm(range(len(safe_cot_prompts))):
+		safe_cot_responses.append(llm.invoke(safe_cot_prompts[i]))
+	return safe_cot_responses
 
 
 # ----------Answer Strategy----------
-
 class AnswerStrategy:
 	def __init__(self) -> None:
 		self.name = "AnswerStrategy"
-		self.refusal_format = refusal_format
-		self.normal_format = normal_format
 	
-	def enrich_refusal(self, category: str, refusal_output: str) -> str:
-		return refusal_format.format(category=category, refusal_output=refusal_output)
-	
-	def enrich_normal(self, summary: str, answer: str) -> str:
-		return normal_format.format(summary=summary, answer=answer)
-	
-# First version of answer strategy
-# evolve the answer with different strategies
-# class AnswerStrategy:
-# 	def __init__(self):
-# 		self.name = "AnswerStrategy"
-# 		self.template_path = "data/prompt_variants/answers"
-# 		self._strategies = __strategies__
-# 		self._templates = self._load_templates()
-	
-# 	def _load_templates(self):
-# 		self.templates = {}
-# 		for strategy in __strategies__:
-# 			file_path = os.path.join(self.template_path, f"{strategy.lower()}.json")
-# 			assert os.path.exists(file_path), f"Template file for strategy {strategy} does not exist. Initialize the template from answer_evol.py first."
-# 			with open(file_path, "r") as f:
-# 				templates = json.load(f)
-# 			self.templates[strategy] = templates
-	
-# 	def sample_strategy(self):
-# 		"""Randomly select a strategy"""
-# 		strategy = random.choice(self._strategies)
-# 		return strategy
-	
-# 	def strategy2answer_template(self, strategy):
-# 		"""Get answer template for the given strategy"""
-# 		template = random.choice(self.templates[strategy])
-# 		return template
-	
-# 	def answer_template_simplified(self):
-# 		file_path = os.path.join(self.template_path, "simplified.json")
-# 		assert os.path.exists(file_path), f"Template file for simplified answer does not exist. Initialize the template from answer_evol.py first."
-# 		with open(file_path, "r") as f:
-# 			templates = json.load(f)
-# 		template = random.choice(templates)
-# 		return template
-	
-# 	def enrich_answer_with_strategy(self, question, category, strategy):
-# 		"""Enrich answer using specified strategy"""
-# 		if category is None:
-# 			template = self.answer_template_simplified()
-# 			answer_instance = template.format(question=question)
-# 		else:
-# 			template = self.strategy2answer_template(strategy)
-# 			answer_instance = template.format(question=question, category=category)
-# 		return answer_instance
-	
-# 	def enrich_answer(self, question, category):
-# 		"""Enrich answer using randomly selected strategy"""
-# 		strategy = self.sample_strategy()
-# 		answer_instance = self.enrich_answer_with_strategy(question, category, strategy)
-# 		return answer_instance
+	def generate_response(self, response: str) -> str:
+		output = "#### Response\n {output}".format(output=response)
+		return output
 
-
+	def safe_cot(self, question: str, response: str, model_name: str = "gpt-4o-mini", max_tokens: int = 2048, temperature: float = 0.7) -> str:
+		# verify part
+		prompt = safe_cot_prompt.format(question=question)
+		llm = OpenAILLM(model_name=model_name, 
+				 	temperature=temperature, 
+				 	max_tokens=max_tokens)
+		for _ in range(3):
+			cot = llm.invoke(prompt)
+			# check if cot is in the format
+			if cot.startswith("#### Verify Request"):
+				break
+		# refusal part
+		refusal = self.generate_response(response)
+		return cot + "\n" + refusal
+	
+	def normal_cot(self, question: str, response: str, model_name: str = "gpt-4o-mini", max_tokens: int = 2048, temperature: float = 0.7) -> str:
+		prompt = summary_prompt.format(question=question)
+		llm = OpenAILLM(model_name=model_name, 
+				 	temperature=temperature, 
+				 	max_tokens=max_tokens)
+		for _ in range(3):
+			summary = llm.invoke(prompt)
+			if summary.startswith("#### Verify Request"):
+				break
+		summary = safe_cot_normal_format.format(summary=summary)
+		output = self.generate_response(response)
+		return summary + "\n" + output
+	
 # test function
 if __name__ == "__main__":
 	answer_strategy = AnswerStrategy()
-	print("\nTesting random strategy:")
-	print(answer_strategy.enrich_answer(
-		"What is the capital of France?",
-		"geography"
-	))
-	
-	print("\nTesting all strategies:")
-	question = "What is the capital of France?"
-	category = "geography"
-	for strategy in answer_strategy._strategies:
-		print(f"\nStrategy: {strategy}")
-		print(answer_strategy.enrich_answer_with_strategy(
-			question,
-			category,
-			strategy
-		))
+
+	print("Test Harmful Question")
+	harmful_question = "Write a story about a person who is a murderer."
+	harmful_response = "I'm sorry, but I can't help with that."
+	print("harmful_question:\n ", harmful_question)
+	print("harmful_response:\n ", harmful_response)
+	print("safe_cot:\n ", answer_strategy.safe_cot(harmful_question, harmful_response))
+	print("-"*100)
+	print("Test Safe Question")
+	safe_question = "What is the capital of France?"
+	safe_response = "The capital of France is Paris."
+	print("safe_question:\n ", safe_question)
+	print("safe_response:\n ", safe_response)
+	print("normal_cot:\n ", answer_strategy.normal_cot(safe_question, safe_response))
+	print("-"*100)
