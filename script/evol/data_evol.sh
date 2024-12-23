ff39436394beb7f2b7ecfe0a5bd5b59ec29c749c
Yang Xianglin <yxlyzxl@gmail.com>
2024-12-23 16:42:33 +0800

    [update]

--- script/evol/data_evol.sh
+++ script/evol/data_evol.sh
@@ -5,5 +5,4 @@
 
 run_id=$RANDOM
 python -m src.evol.question_evol --model_name gpt-4-turbo --num_seed 250 --num_variants 1250 --run_id $run_id
-python -m src.evol.answer_evol --num_variants_per_class 1250 --model_name gpt-4-turbo --run_id $run_id
 python -m src.evol.assemble --run_id $run_id
