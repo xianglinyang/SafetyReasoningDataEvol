009518f823f56c2e33f2442eba70771d0de43874
Yang Xianglin <yxlyzxl@gmail.com>
2025-12-20 21:42:20 +0800

    [imp] dataset class

--- src/data_utils/RobustSCoT_datasets.py
+++ src/data_utils/RobustSCoT_datasets.py
@@ -1,73 +1,47 @@
 import json
 import random
-from typing import Dict
+import os
+from typing import Dict, List
 
 import torch
 import transformers
 from torch.utils.data import Dataset
 import numpy as np
 
+DATA_DIR = "data/processed"
+
+def load_dataset(dataset_name: str):
+    if dataset_name == "STAIR-SFT_diverse":
+        data_path = os.path.join(DATA_DIR, f'STAIR-SFT_diverse.json')
+        with open(data_path, 'r') as f:
+            dataset = json.load(f)
+        new_dataset = []
+        for data in dataset:
+            new_dataset.append({
+                "question": data['instruction'],
+                "answer": data['cot'] + "\n" + data['answer'],
+                "mutations": data['mutations']
+            })
+        return new_dataset
+    else:
+        raise ValueError(f"Dataset {dataset_name} not found")
+
+
 '''Dataset for Safety Reasoning'''
 class SafetyReasoningDataset(Dataset):
     
     def __init__(self, 
-                dataset_name: str,      # 'circuitbreaker'
-                split: str,             # train/val/test
-                model_name: str,         # 'llama3'
-                tokenizer: transformers.PreTrainedTokenizer, 
-                max_length: int = 2048,
-                include_variants=True,
-                include_reasoning=True
-                ):
+        dataset: List[Dict],
+        model_name: str,         # 'llama3'
+        tokenizer: transformers.PreTrainedTokenizer, 
+        max_length: int = 2048,
+    ):
         super(SafetyReasoningDataset, self).__init__()
         self.model_name = model_name
         self.tokenizer = tokenizer
         self.max_length = max_length
-        self.include_variants = include_variants
-        self.include_reasoning = include_reasoning
-        self._load_data(dataset_name, split)
+        self.dataset = dataset
 
-    def _load_data(self, dataset_name, split):
-        # ======================= Circuitbreaker ======================= #
-        data_path = f"data/processed/STAIR-SFT_diverse.json"
-
-        with open(data_path, 'r') as f:
-            dataset = json.load(f)
-        length = len(dataset)
-        if split == 'train':
-            dataset = dataset[:int(length*0.995)]
-        elif split == 'val':
-            dataset = dataset[int(length*0.995):]
-        
-        self.refusal_dataset = list()
-        # original data with evolved question and answer
-        for data in dataset:
-            answer = data['cot']+ "\n"+data['answer']
-            source = data['source']
-            # if source == 'PKU-SafeRLHF':
-            #     prob = np.random.rand()
-            #     if prob < 0.5:
-            #         question = data['instruction']
-            #     else:
-            #         try:
-            #             mutations = data['mutations']
-            #             question = random.choice(mutations)['text']
-            #         except:
-            #             question = data['instruction']
-            # else:
-            #     question = data['instruction']
-            question = data['instruction']
-
-            self.refusal_dataset.append({
-                "question": question,
-                "answer": answer
-            })
-
-        random.shuffle(self.refusal_dataset)
-        print("refusal_dataset length:", len(self.refusal_dataset))
-    
-
-    
     def _format_data(self, question, answer):
         # Format messages for refusal dataset and retain dataset
         messages = [
@@ -98,39 +72,50 @@ class SafetyReasoningDataset(Dataset):
                                                 padding=False)
         labels[:encodings_wo_assistant.input_ids.shape[1]] = -100   
         model_inputs['labels'] = labels
-
         return model_inputs.copy()
 
     def __len__(self):
-        return len(self.refusal_dataset)
+        return len(self.dataset)
 
     def __getitem__(self, i) -> Dict[str, torch.Tensor]:
         # Get items from both datasets
-        refusal_example = self.refusal_dataset[i % len(self.refusal_dataset)]  
-        refusal_question = refusal_example['question']
-        refusal_answer = refusal_example['answer']
-        refusal_inputs = self._format_data(refusal_question, refusal_answer)
+        item = self.dataset[i]
+        question = item['question']
+        answer = item['answer']
+        mutation = item['mutation'] if 'mutation' in item else None
+        normal_inputs = self._format_data(question, answer)
+
         model_inputs = dict(
-            input_ids=refusal_inputs['input_ids'],
-            attention_mask=refusal_inputs['attention_mask'],
-            labels=refusal_inputs['labels']
+            input_ids=normal_inputs['input_ids'],
+            attention_mask=normal_inputs['attention_mask'],
+            labels=normal_inputs['labels'],
         )
+        if mutation is not None:
+            adv_inputs = self._format_data(mutation, answer)
+            model_inputs['adv_input_ids'] = adv_inputs['input_ids']
+            model_inputs['adv_attention_mask'] = adv_inputs['attention_mask']
+            model_inputs['adv_labels'] = adv_inputs['labels']
+        else:
+            model_inputs['adv_input_ids'] = normal_inputs['input_ids'].clone()
+            model_inputs['adv_attention_mask'] = normal_inputs['attention_mask'].clone()
+            model_inputs['adv_labels'] = normal_inputs['labels'].clone()
+        
         return model_inputs
 
 
 if __name__ == "__main__":
     from transformers import AutoTokenizer
+    dataset = json.load(open("data/processed/STAIR-SFT_diverse.json", "r"))
+
+
     tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
     tokenizer.pad_token = tokenizer.eos_token
     train_dataset = SafetyReasoningDataset(
         model_name="meta-llama/Llama-3.1-8B-Instruct",
-        dataset_name="STAIR-SFT_diverse",
-        split="train",
+        dataset=dataset,
         tokenizer=tokenizer,
         max_length=2048,
-        include_variants=True,
-        include_reasoning=True
     )
-    print(train_dataset.refusal_dataset[0])
+    print(train_dataset.dataset[0])
 
     
