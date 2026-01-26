dc7b0e3da5a99e25dca240a004f73298f633fe84
Yang Xianglin <yxlyzxl@gmail.com>
2026-01-26 21:31:06 +0800

    minor

--- script/evol/data_evol.sh
+++ script/evol/data_evol.sh
@@ -2,6 +2,5 @@
 
 
 run_id=$RANDOM
-python -m src.evol.assemble --dataset_name STAIR-SFT --llm_name openai/gpt-4o-mini --num 18 --demo_selected_strategy diverse --run_id $run_id
 
-python -m src.evol.assemble --dataset_name R2D-R1 --llm_name openai/gpt-4o-mini --num 18 --demo_selected_strategy diverse --run_id $run_id
+python -m src.evol.question_mutation_main --dataset_name circuit_breakers_train --llm_name openai/gpt-4o-mini --num_variants 18 --alpha 0.5 --demo_selected_strategy diverse --run_id $run_id
