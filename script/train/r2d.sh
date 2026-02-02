49c9981010eef16d4d4fda45f56f61bf00ab3d33
Yang Xianglin <yxlyzxl@gmail.com>
2026-02-02 17:26:42 +0800

    [fix] fix hyperparameters

---
+++ script/train/r2d.sh
@@ -0,0 +1,35 @@
+export CUDA_VISIBLE_DEVICES=0
+
+OUTPUTS_PATH="/data2/xianglin/RobustSCoT"
+mkdir -p ${OUTPUTS_PATH}/r2d_outputs
+
+
+MODEL_NAME="DeepSeek-R1-Distill-Qwen-7B"
+MODEL_NAME_OR_PATH="deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
+
+#--multi_gpu --num_processes 4
+accelerate launch \
+    --num_processes 1 \
+    --mixed_precision bf16 \
+    src/Baselines/R2D/main.py \
+    --model_name_or_path ${MODEL_NAME_OR_PATH} \
+    --use_lora \
+    --output_dir ${OUTPUTS_PATH}/r2d_outputs/${MODEL_NAME}-r2d \
+    --bf16 \
+    --num_train_epochs 1 \
+    --save_strategy steps \
+    --save_steps 2000 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --learning_rate 2e-4 \
+    --lr_scheduler_type cosine \
+    --warmup_ratio 0.02 \
+    --report_to none \
+    --gradient_checkpointing \
+    --ddp_find_unused_parameters False \
+    --logging_steps 10 \
+    --r 64 \
+    --lora_alpha 64 \
+    --modules_to_save embed_tokens lm_head \
+    --target_modules q_proj k_proj v_proj up_proj down_proj \
+
